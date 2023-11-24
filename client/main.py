cb418b22909cd32630dc771e0e26bae895be48b2
Zilanlann <666zilan666@gmail.com>
2023-11-24 10:09:34 +0800

    完善入口

--- client/main.py
+++ client/main.py
@@ -1,13 +1,7 @@
 import sys
 
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QMessageBox, QLabel, \
-    QTimeEdit, QDialog, QDialogButtonBox
+from PyQt6.QtWidgets import QApplication
 from login import LoginWindow
-from client import ClientThread
-from th_graph import PlotWindow
-from link_serial import *
-from main_window import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
